af69b491c692b90515648ec89ec2248bfdc751b7
Luis Alberto <fernandezc.alberto@gmail.com>
2017-05-05 12:36:01 +0200

    fixing _https_ problems

--- js/services/dataService.js
+++ js/services/dataService.js
@@ -1,12 +1,12 @@
 angular.module('mainApp')
     .factory('dataService', function ($http) {
       function getMoviesArray (_movieName) {
-        var url = 'http://api.themoviedb.org/3/search/movie?api_key=d6ceefa77210945fb936085a6798e7a6&query=' + _movieName
+        var url = 'https://api.themoviedb.org/3/search/movie?api_key=d6ceefa77210945fb936085a6798e7a6&query=' + _movieName
         return $http.get(url)
       }
 
       function getTVArray (_tvName) {
-        var url = 'http://api.themoviedb.org/3/search/tv?api_key=d6ceefa77210945fb936085a6798e7a6&query=' + _tvName
+        var url = 'https://api.themoviedb.org/3/search/tv?api_key=d6ceefa77210945fb936085a6798e7a6&query=' + _tvName
         return $http.get(url)
       }
 
