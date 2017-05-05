4d3fe357321192b7c445e304d3f4408e41790f0a
Jaume Regas <jaumeregas1@gmail.com>
2017-05-05 12:03:42 +0200

    details page styled

--- js/controllers/detailsController.js
+++ js/controllers/detailsController.js
@@ -5,9 +5,7 @@ angular.module('mainApp')
         dataService.getMovieById(idMovie)
             .then(function(response) {
                 var detailsMovie = response.data               
-                if (detailsMovie.backdrop_path === null) {
-                    $scope.imageMovie = detailsMovie.poster_path
-                } else { $scope.imageMovie = detailsMovie.backdrop_path }
+                $scope.imageMovie = detailsMovie.backdrop_path
                 $scope.movie = detailsMovie
                 console.log($scope.movie)
             })
